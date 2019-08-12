5f5d179a928315f13fffe165e533d253cca5e347
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-12 08:28:40 +0700

    up file count char in string

---
+++ count-char-in-string.js
@@ -0,0 +1,8 @@
+function countChar() {
+    let string = "Hello world!";
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        count++;
+    }
+    alert('Chuoi ' + string + ' Co so ky tu la: ' + count);
+}
